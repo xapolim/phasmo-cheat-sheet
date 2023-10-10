bc35b9d4c22c79fbe9594e41378b07538c7ff1a7
xapolim <49494437+xapolim@users.noreply.github.com>
2023-10-10 15:40:36 -0300

    Update ghost_template-v4.js

--- scripts-v3/ghost_template-v4.js
+++ scripts-v3/ghost_template-v4.js
@@ -45,12 +45,7 @@ class Ghost {
                 <div class="ghost_name">${data.ghost}</div>
                 <div class="ghost_hunt_info">
                     <div class="ghost_hunt ${parseInt(data.hunt_sanity) > 50 ?'high':parseInt(data.hunt_sanity) < 50 ? 'low':'average'}"><img src="imgs/sanity.png">${data.hunt_sanity}</div>
-                    <div class="ghost_speed">
-                        <img src="imgs/footsteps.png">
-                        <div class="ghost_speed_values">
-                            ${this.toNumStr(data.min_speed)} <span class="ms">m/s</span> <span class="sound" onclick="toggleSound(${data.min_speed},'${data.ghost}0')">&#128266;</span>${data.max_speed == null ? '' : data.speed_is_range?' - ':' | '}${data.max_speed == null ? '' : this.toNumStr(data.max_speed)+' <span class="ms">m/s</span> <span class="sound" onclick="toggleSound('+data.max_speed+',\''+data.ghost+'1\')">&#128266;</span>'}${data.alt_speed == null ? '' : '<br>('+this.toNumStr(data.alt_speed)+' <span class="ms">m/s</span> <span class="sound" onclick="toggleSound('+data.alt_speed+',\''+data.ghost+'2\')">&#128266;</span>)'}
-                        </div>
-                    </div>
+                    
                 </div>
                 <div class="ghost_evidence">
                     <div class="ghost_evidence_item" ${data.evidence[0] in evi_color ? 'style=\"color:' + evi_color[data.evidence[0]] + ' !important;\"' : ''}><img src="${evi_icons[data.evidence[0]]}">${data.evidence[0]}</div>
